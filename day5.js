b82f20104d3a2af0c854ffddf8144966b5534a30
vidhyavkm <85151120+vidhyavkm@users.noreply.github.com>
2022-07-22 13:00:30 +0530

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,135 @@
+// // normal function without parameters
+
+// function foo(){
+   
+//     return ("hello world");
+//     console.log("hello world"); 
+// }
+// console.log(foo());
+ 
+// // normal function with parameters
+// function calculate(a,b){
+//     return (a+b);
+    // console.log("hello world");
+// }
+// console.log(calculate(13,30));
+
+// // named function
+// function multiply(x,y){
+    
+// }
+
+// // x=20 y=30
+
+//anonymous function
+// var a = [1,2,3,4]
+
+// var nums1 = [1, 2, 9]
+// var nums2 = [3, 4, 7]
+
+
+//  var median = function (a,b){
+//     return (a+b);
+//  }
+//  console.log(sum(500,500));
+
+// function difference (a,b){
+//     return (a-b);
+// }
+// console.log(difference(5,6));
+
+// var multiply = function (x,b) {
+//     return(x*b);
+// }
+// console.log(multiply(64,96));
+
+// IIFE function
+// (function sum(a,b){
+//   console.log(a+b)
+// })
+// (40,50);
+
+// (function bar(a,b) {
+//     console.log(a+b);
+// })
+// (20,30);
+
+// arrow function
+ 
+// var res = (a,b) => {
+//     // console.log("hello world")
+//     return (a+b);
+// }
+// console.log(res(20,30));
+
+// find the average of the marks of 5 students?
+// u can use the array as an argument
+
+// var marks = [40,50,0,90,10];
+
+// let sum = 0;
+
+// for (let mark of marks){
+//     sum = sum + mark;
+// }
+// let avg = sum / marks.length;
+
+// console.log(avg);
+
+// function getAverage(marks){
+// // var marks = [40,50,0,90,10];
+
+// let sum = 0;
+
+// for (let mark of marks){
+//     sum = sum + mark;
+// }
+// let avg = sum / marks.length;
+// return avg;
+// }
+
+// console.log(getAverage([40,50,0,90,10]));
+
+// var getAverage = function(marks){
+//     // var marks = [40,50,0,90,10];
+    
+//     let sum = 0;
+    
+//     for (let mark of marks){
+//         sum = sum + mark;
+//     }
+//     let avg = sum / marks.length;
+//     return avg;
+//     }
+    
+//     console.log(getAverage([40,50,0,90,10]));
+
+//     var getAverage = (marks) => {
+//         // var marks = [40,50,0,90,10];
+        
+//         let sum = 0;
+        
+//         for (let mark of marks){
+//             sum = sum + mark;
+//         }
+//         let avg = sum / marks.length;
+//         return avg;
+//         }
+        
+//         console.log(getAverage([40,50,0,90,10]));
+    
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const odds = arr.filter(number => {
+//   return number % 2 !== 0;
+// });
+
+// console.log(odds);
+
+
+
+// (function oddnumber(number) {
+//     console.log(number.filter(n=>n%2));
+//     })
+//     (arr);
+    
